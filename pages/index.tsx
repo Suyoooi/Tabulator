2701fc8be7a0116d424ffb9523b60e8417c6c2ec
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2023-06-09 16:32:37 +0900

    feat: gridComponent 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import ExportTest from "@/components/exportTest";
+import GridComponent from "@/components/gridComponent";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
+      <GridComponent />
       <ExportTest />
     </>
   );
