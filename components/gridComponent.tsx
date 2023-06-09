0f2978cfdf76d41061bd175be79d5daf5320f635
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2023-06-09 16:31:19 +0900

    feat: tabulator 기능 및 스타일 적용

--- components/gridComponent.tsx
+++ components/gridComponent.tsx
@@ -38,47 +38,72 @@ const GridComponent = () => {
     }
   };
 
-  const tableRef = useRef<ReactTabulator | null>(null);
-
-  const options: ReactTabulatorOptions = {
-    pagination: true,
-    paginationSize: 10,
-    paginationSizeSelector: [5, 10],
-    layout: "fitData",
-  };
-
   const columns = [
-    { title: "Name", field: "name" },
-    { title: "Age", field: "age" },
-    { title: "Email", field: "email" },
+    { title: "Name", field: "name", width: 150 },
+    { title: "Age", field: "age", hozAlign: "left", formatter: "progress" },
+    { title: "Favourite Color", field: "col" },
+    { title: "Date Of Birth", field: "dob", hozAlign: "center" },
+    { title: "Rating", field: "rating", hozAlign: "center", formatter: "star" },
+    {
+      title: "Passed?",
+      field: "passed",
+      hozAlign: "center",
+      formatter: "tickCross",
+    },
+  ];
+
+  const initialData: TableDataItem[] = [
+    { id: 1, name: "Oli Bob", age: "12", col: "red", dob: "" },
+    { id: 2, name: "Mary May", age: "1", col: "blue", dob: "14/05/1982" },
+    {
+      id: 3,
+      name: "Christine Lobowski",
+      age: "42",
+      col: "green",
+      dob: "22/05/1982",
+    },
+    {
+      id: 4,
+      name: "Brendon Philips",
+      age: "125",
+      col: "orange",
+      dob: "01/08/1980",
+    },
+    {
+      id: 5,
+      name: "Margret Marmajuke",
+      age: "16",
+      col: "yellow",
+      dob: "31/01/1999",
+    },
   ];
 
   useEffect(() => {
-    if (tableRef.current && tableRef.current.table) {
-      tableRef.current.table.setData(data);
-    }
-  }, [data]);
+    setTableData(initialData);
+  }, []);
 
   return (
     <div>
-      <ReactTabulator
-        ref={tableRef}
-        columns={columns}
-        data={data}
-        options={options}
-      />
-      <button
-        style={{ backgroundColor: "lightgray", width: 100, borderRadius: 10 }}
-        onClick={handleExportCSV}
-      >
-        Export as CSV
-      </button>
-      <button
-        style={{ backgroundColor: "lightgray", width: 100, borderRadius: 10 }}
-        onClick={handleExportXLSX}
-      >
-        Export as XLSX
-      </button>
+      <div className="table-responsive">
+        <ReactTabulator
+          ref={tableRef}
+          data={tableData}
+          columns={columns}
+          layout={"fitData"}
+        />
+        <button
+          style={{ backgroundColor: "lightgray", width: 100, borderRadius: 10 }}
+          onClick={handleExportCSV}
+        >
+          Export as CSV
+        </button>
+        <button
+          style={{ backgroundColor: "lightgray", width: 100, borderRadius: 10 }}
+          onClick={handleExportXLSX}
+        >
+          Export as XLSX
+        </button>
+      </div>
     </div>
   );
 };
