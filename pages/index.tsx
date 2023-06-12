4144b6f1b1843aee5b00247d11465e08877c9dc8
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2023-06-12 10:48:12 +0900

    feat: emsGridTest 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,14 @@ import { Inter } from "next/font/google";
 import ExportTest from "@/components/exportTest";
 import GridComponent from "@/components/gridComponent";
 import TabulatorGrid from "@/components/tabulatorGrid";
+import EmsGridTest from "@/components/emsGridTest";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
+      <EmsGridTest />
       <TabulatorGrid />
       <GridComponent />
       <ExportTest />
