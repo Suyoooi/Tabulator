c1326e5a84d0fa92cf68fa7bb8d23b4d3917b141
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2023-06-12 13:47:59 +0900

    feat: port 변경

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "http://192.168.10.9:38080/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
