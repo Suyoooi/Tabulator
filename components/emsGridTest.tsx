e6bf4ffb9a477cd82e69c2a556d1b2737ba4194d
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2023-06-14 18:03:31 +0900

    fix: checkbox 추가

--- components/emsGridTest.tsx
+++ components/emsGridTest.tsx
@@ -10,79 +10,47 @@ interface TableDataItem {
   srvrAlias: string;
   fabLocCd: string;
   fabCd: string;
+  _children?: TableDataItem[];
 }
 
-const EmsGridTest = () => {
+const emsGridTest = () => {
   const tableRef = useRef<ReactTabulator | null>(null);
   const [tableData, setTableData] = useState<TableDataItem[]>([]);
 
-  // csv 형식으로 저장
-  const handleExportCSV = () => {
-    if (tableRef.current && tableRef.current.table) {
-      const table = tableRef.current.table;
-      table.download("csv", "data.csv");
-    }
-  };
-
-  // xlsx 형식으로 저장
-  const handleExportXLSX = () => {
-    if (tableRef.current && tableRef.current.table) {
-      const table = tableRef.current.table;
-      const data = table.getData();
-      const worksheet = XLSX.utils.json_to_sheet(data);
-      const workbook = XLSX.utils.book_new();
-      XLSX.utils.book_append_sheet(workbook, worksheet, "Data");
-      const excelBuffer = XLSX.write(workbook, {
-        bookType: "xlsx",
-        type: "array",
-      });
-      const excelData = new Blob([excelBuffer], {
-        type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-      });
-      const excelUrl = URL.createObjectURL(excelData);
-      const link = document.createElement("a");
-      link.href = excelUrl;
-      link.download = "data.xlsx";
-      link.click();
-    }
-  };
-
-  // Add Data 버튼을 클릭했을 때 실행되는 함수
-  const handleAddData = () => {
-    const newId = tableData.length + 1;
-    const newData: TableDataItem = {
-      id: newId,
-      emsQueNm: "",
-      collectDate: "",
-      srvrAlias: "",
-      fabLocCd: "",
-      fabCd: "",
-    };
-    const updatedData = [...tableData, newData];
-    setTableData(updatedData);
-  };
-
   const columns = [
-    { title: "emsQueNm", field: "emsQueNm", hozAlign: "center", editor: true },
+    {
+      title: "",
+      width: 40,
+      formatter: "rowSelection",
+      titleFormatter: "rowSelection",
+      hozAlign: "center",
+      headerSort: false,
+      cssClass: "text-center",
+      cellClick: function (cell: {
+        getRow: () => {
+          toggleSelect: { (): void; new (): any };
+        };
+      }) {
+        cell.getRow().toggleSelect();
+      },
+    },
+    { title: "emsQueNm", field: "emsQueNm", hozAlign: "center" },
     {
       title: "collectDate",
       field: "collectDate",
       hozAlign: "center",
-      editor: true,
     },
     {
       title: "srvrAlias",
       field: "srvrAlias",
       hozAlign: "center",
-      editor: true,
     },
     {
       title: "fabLocCd",
       field: "fabLocCd",
       hozAlign: "center",
-      editor: true,
     },
-    { title: "fabCd", field: "fabCd", hozAlign: "center", editor: true },
+    { title: "fabCd", field: "fabCd", hozAlign: "center" },
   ];
 
   const initialData: TableDataItem[] = [
@@ -125,12 +93,54 @@ const EmsGridTest = () => {
       srvrAlias: "31/01/1999",
       fabLocCd: "0",
       fabCd: "D11",
+      _children: [
+        {
+          title: "",
+          width: 40,
+          formatter: "rowSelection",
+          titleFormatter: "rowSelection",
+          hozAlign: "center",
+          headerSort: false,
+          cssClass: "text-center",
+          cellClick: function (cell: {
+            getRow: () => {
+              toggleSelect: { (): void; new (): any };
+            };
+          }) {
+            cell.getRow().toggleSelect();
+          },
+        },
+        {
+          id: 6,
+          emsQueNm: "Gana",
+          collectDate: "16",
+          srvrAlias: "31/01/1999",
+          fabLocCd: "0",
+          fabCd: "D11",
+        },
+        {
+          id: 7,
+          emsQueNm: "Gana",
+          collectDate: "16",
+          srvrAlias: "31/01/1999",
+          fabLocCd: "0",
+          fabCd: "D11",
+        },
+      ],
     },
   ];
 
   useEffect(() => {
     setTableData(initialData);
   }, []);
+
+  // Tree와 Grouping 옵션 추가
+  const options = {
+    layout: "fitData",
+    dataTree: true,
+    dataTreeStartExpanded: true,
+    groupBy: "fabLocCd",
+  };
 
   return (
     <div>
@@ -139,29 +149,11 @@ const EmsGridTest = () => {
           ref={tableRef}
           data={tableData}
           columns={columns}
-          layout={"fitData"}
+          options={options}
         />
-        <button
-          style={{ backgroundColor: "lightgray", width: 150, borderRadius: 10 }}
-          onClick={handleAddData}
-        >
-          Add Data
-        </button>
-        <button
-          style={{ backgroundColor: "lightgray", width: 150, borderRadius: 10 }}
-          onClick={handleExportCSV}
-        >
-          Export as CSV
-        </button>
-        <button
-          style={{ backgroundColor: "lightgray", width: 150, borderRadius: 10 }}
-          onClick={handleExportXLSX}
-        >
-          Export as XLSX
-        </button>
       </div>
     </div>
   );
 };
 
-export default EmsGridTest;
+export default emsGridTest;
