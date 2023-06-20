f92bf93a26c78f7c19d23b5818f2e8aa926a9e65
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2023-06-20 15:15:10 +0900

    feat: MultiBothSearchDropdown 파일 생성

---
+++ components/dropdown/Multi/MultiBothSearchDropdown.tsx
@@ -0,0 +1,271 @@
+import React, { useEffect, useRef, useState } from "react";
+import { ReactTabulator } from "react-tabulator";
+import { CellComponent, RowComponent } from "tabulator-tables";
+import "react-tabulator/lib/styles.css";
+
+interface TableDataItem {
+  id: number;
+  server: string;
+  value: string;
+}
+
+const initialData: TableDataItem[] = [
+  {
+    id: 1,
+    server: "EMS1",
+    value: "EMS1",
+  },
+  {
+    id: 2,
+    server: "EMS2",
+    value: "EMS2",
+  },
+  {
+    id: 3,
+    server: "EMS3",
+    value: "EMS3",
+  },
+  {
+    id: 4,
+    server: "EMS4",
+    value: "EMS4",
+  },
+  {
+    id: 5,
+    server: "EMS5",
+    value: "EMS5",
+  },
+  {
+    id: 6,
+    server: "EMS6",
+    value: "EMS6",
+  },
+  {
+    id: 7,
+    server: "EMS7",
+    value: "EMS7",
+  },
+];
+
+const MultiBothSearchDropdown = () => {
+  const tableRef = useRef<ReactTabulator | null>(null);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [selectedData, setSelectedData] = useState<TableDataItem[]>([]);
+  const [dropdownMenu, setDropdownMenu] = useState<boolean>(false);
+
+  console.log(selectedData);
+
+  //   선택한 서버에 대한 변수
+  const selectedServerNames = selectedData.map((option) => option.server);
+  const selectedServerCount = selectedData.length;
+
+  const columns = [
+    {
+      title: "",
+      width: 40,
+      formatter: "rowSelection",
+      titleFormatter: "rowSelection",
+      hozAlign: "center",
+      headerSort: false,
+      cssClass: "text-center",
+      cellClick: function (cell: CellComponent) {
+        const row = cell.getRow();
+        row.toggleSelect();
+      },
+    },
+    { title: "server", field: "server", hozAlign: "center" },
+  ];
+
+  // useEffect에서 전체 데이터를 선택한 배열이 반환됨
+  // useEffect(() => {
+  //   setSelectedData(initialData);
+  // }, []);
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredData = searchTerm
+    ? initialData.filter((item) =>
+        item.server.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    : initialData;
+
+  const options = {
+    layout: "fitColumns",
+    printAsHtml: true,
+    printVisibleRowsOnly: true,
+    movableColumns: true,
+  };
+
+  // 데이터 확인용 alert 추가
+  // 체크박스 선택 여부에 따라 selectedData 배열 업데이트
+  const handleConfirm = () => {
+    const selectedRows = tableRef.current?.table.getSelectedRows() || [];
+    const selectedCount = selectedRows.length;
+
+    if (selectedCount > 0) {
+      const selectedItems = selectedRows.map((row: RowComponent) =>
+        row.getData()
+      );
+      setSelectedData(selectedItems);
+      const selectedServerNames = selectedItems.map(
+        (item: TableDataItem) => item.server
+      );
+      alert(
+        `서버 ${selectedCount}건을 선택했습니다. 선택한 서버: ${selectedServerNames.join(
+          ", "
+        )}`
+      );
+    } else {
+      setSelectedData([]);
+      alert("No servers selected");
+    }
+    setDropdownMenu(false);
+    console.log(selectedData);
+  };
+
+  const handleDropdownVisible = () => {
+    setSearchTerm("");
+    setDropdownMenu(!dropdownMenu);
+  };
+
+  const handleCancelClick = () => {
+    setSelectedData([]);
+    setDropdownMenu(false);
+  };
+
+  const clearSearch = () => {
+    setSearchTerm("");
+    tableRef.current?.table.clearFilter();
+  };
+
+  const dropdownText =
+    selectedData.length > 0
+      ? `${selectedServerNames[0]}외 ${selectedData.length - 1}건`
+      : "=== EMS ===";
+
+  const selectedOptionName =
+    selectedData.length === 1 ? selectedData[0].value : "";
+
+  return (
+    <div style={{ zIndex: 10 }}>
+      {/* === 입력 창 === */}
+      <div style={{ display: "flex" }}>
+        {/* <div
+          style={{
+            backgroundColor: "lightGrey",
+            width: 150,
+            height: 24,
+            cursor: "pointer",
+          }}
+        >
+          {selectedData.length === 1 ? selectedOptionName : dropdownText}
+        </div> */}
+        <input
+          style={{
+            backgroundColor: "lightGrey",
+            width: 150,
+            height: 24,
+          }}
+        ></input>
+        <button
+          onClick={handleDropdownVisible}
+          style={{
+            backgroundColor: "grey",
+            paddingLeft: 4,
+            paddingRight: 4,
+          }}
+        >
+          ▽
+        </button>
+      </div>
+
+      {/* === 드롭 다운 메뉴 === */}
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "#E5E5E5",
+          width: 200,
+          zIndex: 100,
+        }}
+      >
+        {dropdownMenu ? (
+          <div style={{ width: 200, backgroundColor: "#E5E5E5" }}>
+            <div>
+              <input
+                style={{ color: "black", width: 150 }}
+                type="text"
+                placeholder="Search"
+                value={searchTerm}
+                onChange={handleSearchChange}
+              />
+              <button
+                onClick={clearSearch}
+                style={{ paddingLeft: 4, paddingRight: 4 }}
+              >
+                Clear
+              </button>
+            </div>
+            <div
+              style={{
+                width: 200,
+                height: 200,
+                overflow: "scroll",
+                overflowX: "hidden",
+                backgroundColor: "#E5E5E5",
+              }}
+            >
+              {filteredData.length > 0 ? (
+                <ReactTabulator
+                  ref={tableRef}
+                  data={filteredData}
+                  columns={columns}
+                  options={options}
+                  layout={"fitData"}
+                />
+              ) : (
+                <div>
+                  <ReactTabulator
+                    ref={tableRef}
+                    data={filteredData}
+                    columns={columns}
+                    options={options}
+                    layout={"fitData"}
+                  />
+                  <div>Data가 존재하지 않습니다.</div>
+                </div>
+              )}
+            </div>
+            <div>
+              <button
+                style={{
+                  backgroundColor: "lightGrey",
+                  borderRadius: 10,
+                  width: 60,
+                }}
+                onClick={handleConfirm}
+              >
+                Confirm
+              </button>
+              <button
+                style={{
+                  backgroundColor: "lightGrey",
+                  borderRadius: 10,
+                  width: 60,
+                }}
+                onClick={handleCancelClick}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MultiBothSearchDropdown;
