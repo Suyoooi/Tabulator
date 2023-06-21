6c0fb108f3ca1aa103fad0f483fce789ea8e12fe
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2023-06-21 17:33:04 +0900

    feat: Grid 드롭다운 Add

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,50 +1,21 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import ExportTest from "@/components/exportTest";
-import GridComponent from "@/components/gridComponent";
-import TabulatorGrid from "@/components/tabulatorGrid";
-import EmsGridTest from "@/components/emsGridTest";
-import DataAppend from "@/components/data/dataAppend";
-import DataPreAppend from "@/components/data/dataPreAppend";
-import MultiSelectDropdown from "@/components/dropdown/Multi/MultiSelectDropdown";
-import SingleSelectDropdown from "@/components/dropdown/Single/SingleSelectDropdown";
-import DropdownTest from "@/components/dropdown/dropdownTest";
-import TabulatorDataTree from "@/components/tabulatorDataTree";
-import BasicDropdown from "@/components/dropdown/BasicDropdown";
 import SimpleDropdown from "@/components/dropdown/SimpleDropdown";
 import SingleTabulatorDropdown from "@/components/dropdown/Single/SingleTabultorDropdown";
 import MultiGroupDropdown from "@/components/dropdown/Multi/MultiGroupDropdown";
-import MultiSearchDropdown from "@/components/dropdown/Multi/MultiSearchDropdown";
 import SimpleCategoryDropdown from "@/components/dropdown/SimpleCategoryDropdown";
-import SingleSearchDropdown from "@/components/dropdown/Single/SingleSearchDropdown";
 import MultiTabulatorDropdown from "@/components/dropdown/Multi/MultiTabulatorDropdown";
 import MultiBothSearchDropdown from "@/components/dropdown/Multi/MultiBothSearchDropdown";
+import GridDropdown from "@/components/Grid/GridDropdown";
 
 const inter = Inter({ subsets: ["latin"] });
-
-const emsOptions = [
-  { value: "ems1", label: "ems1" },
-  { value: "ems2", label: "ems2" },
-  { value: "ems3", label: "ems3" },
-];
-
-const options = [
-  { value: "option1", label: "Option 1" },
-  { value: "option2", label: "Option 2" },
-  { value: "option3", label: "Option 3" },
-];
-
-const subMenuOptions = [
-  { value: "subOption1", label: "Sub Option 1" },
-  { value: "subOption2", label: "Sub Option 2" },
-  { value: "subOption3", label: "Sub Option 3" },
-];
 
 export default function Home() {
   return (
     <>
-      <div style={{ height: "800px", background: "ivory" }}>
-        <div style={{ display: "flex", gap: 10 }}>
+      <div style={{ height: "800px", background: "#f7ddd1" }}>
+        <div style={{ display: "flex", gap: 50, marginBottom: 100 }}>
+          <br />
           <br />
           <SimpleDropdown />
           <br />
@@ -52,30 +23,16 @@ export default function Home() {
           <br />
           <MultiGroupDropdown />
           <br />
+        </div>
+        <div style={{ display: "flex", gap: 60, marginBottom: 100 }}>
           <br />
           <SingleTabulatorDropdown />
           <br />
           <MultiTabulatorDropdown />
           <br />
           <MultiBothSearchDropdown />
-          {/* <MultiSearchDropdown /> */}
-          {/* <BasicDropdown /> */}
-          {/* <SimpleDropdown /> */}
           <br />
-          {/* <SingleSelectDropdown /> */}
-          <br />
-          {/* <SingleTabulatorDropdown /> */}
-          <br />
-          {/* <MultiSelectDropdown /> */}
-          <br />
-          {/* <MultiTabulatorDropdown /> */}
-          {/* <TabulatorDataTree /> */}
-          {/* <DataAppend /> */}
-          {/* <DataPreAppend /> */}
-          {/* <EmsGridTest /> */}
-          {/* <TabulatorGrid /> */}
-          {/* <GridComponent /> */}
-          {/* <ExportTest /> */}
+          <GridDropdown />
         </div>
       </div>
     </>
