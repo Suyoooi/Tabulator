ef30d49eb659f677d610dfb720277526a316af79
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2023-06-22 15:10:46 +0900

    feat: grid component 생성

--- components/Grid/Grid.tsx
+++ components/Grid/Grid.tsx
@@ -1,71 +1,55 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
+
+interface IDataItem {
+  id: number;
+  [key: string]: string | number;
+}
 
 interface GridComponentProps {
-  data: TableDataItem[];
+  data: IDataItem[];
+  headers: string[];
   fieldLabel: string;
 }
 
-interface TableDataItem {
-  id: number;
-  server: string;
-}
-
-const GridComponent: React.FC<GridComponentProps> = ({ data, fieldLabel }) => {
-  const [selectedData, setSelectedData] = useState<TableDataItem[]>([]);
+const Grid: React.FC<GridComponentProps> = ({ data, headers, fieldLabel }) => {
   const [selectAll, setSelectAll] = useState(false);
-  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [filteredData, setFilteredData] = useState(data);
 
-  // 체크 박스 선택
-  const handleCheckboxChange = (item: TableDataItem) => {
-    const isChecked = selectedData.some(
-      (selectedItem) => selectedItem.id === item.id
-    );
-    if (isChecked) {
-      setSelectedData((prevSelectedData) =>
-        prevSelectedData.filter((selectedItem) => selectedItem.id !== item.id)
-      );
-      setSelectAll(false);
+  const handleSelectAllChange = () => {
+    if (selectAll) {
+      setSelectedItems([]);
     } else {
-      setSelectedData((prevSelectedData) => [...prevSelectedData, item]);
-      if (selectedData.length + 1 === data.length) {
-        setSelectAll(true);
-      }
+      setSelectedItems(filteredData.map((item) => item.id));
+    }
+    setSelectAll(!selectAll);
+  };
+
+  const checkboxChecked = (item: IDataItem) => selectedItems.includes(item.id);
+
+  const handleCheckboxChange = (item: IDataItem) => {
+    if (checkboxChecked(item)) {
+      setSelectedItems(selectedItems.filter((id) => id !== item.id));
+    } else {
+      setSelectedItems([...selectedItems, item.id]);
     }
   };
 
-  // 전체 선택
-  const handleSelectAllChange = () => {
-    const newSelectAll = !selectAll;
-    setSelectAll(newSelectAll);
-    setSelectedData(newSelectAll ? data : []);
-  };
-
-  // 데이터 필터링
-  const filteredData = searchTerm
-    ? data.filter((item) =>
-        item.server.toLowerCase().includes(searchTerm.toLowerCase())
-      )
-    : data;
-
-  // 선택한 체크 박스
-  const checkboxChecked = (item: TableDataItem) =>
-    selectedData.some((selectedItem) => selectedItem.id === item.id);
-
   return (
-    <div style={{ width: 300 }}>
-      <table className="w-full border-collapse">
-        <thead>
-          <tr style={{ backgroundColor: "grey" }}>
-            <th className="w-12">
-              <input
-                type="checkbox"
-                checked={selectAll}
-                onChange={handleSelectAllChange}
-              />
-            </th>
+    <table className="table-fixed">
+      <thead>
+        <tr style={{ backgroundColor: "grey" }}>
+          <th className="w-12">
+            <input
+              type="checkbox"
+              checked={selectAll}
+              onChange={handleSelectAllChange}
+            />
+          </th>
+          {headers.map((header) => (
             <th
+              key={header}
               className="border-b"
-              colSpan={2}
               style={{
                 color: "black",
                 paddingLeft: 10,
@@ -73,43 +57,44 @@ const GridComponent: React.FC<GridComponentProps> = ({ data, fieldLabel }) => {
                 backgroundColor: "grey",
               }}
             >
-              {/* 필드 명 라벨을 출력 */}
-              {fieldLabel}
+              {header}
             </th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredData.map((item, index) => (
-            <tr
-              key={item.id}
-              style={{
-                backgroundColor: index % 2 === 0 ? "lightGrey" : "grey",
-              }}
-            >
-              <td className="text-center">
-                <input
-                  type="checkbox"
-                  checked={checkboxChecked(item)}
-                  onChange={() => handleCheckboxChange(item)}
-                />
-              </td>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {filteredData.map((item, index) => (
+          <tr
+            key={item.id}
+            style={{
+              backgroundColor: index % 2 === 0 ? "lightGrey" : "grey",
+            }}
+          >
+            <td className="text-center">
+              <input
+                type="checkbox"
+                checked={checkboxChecked(item)}
+                onChange={() => handleCheckboxChange(item)}
+              />
+            </td>
+            {headers.map((header) => (
               <td
+                key={header}
                 className="border-b"
-                colSpan={2}
                 style={{
                   color: "black",
                   paddingLeft: 10,
                   borderLeft: "2px solid black",
                 }}
               >
-                {item.server}
+                {item[header]}
               </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
-export default GridComponent;
+export default Grid;
